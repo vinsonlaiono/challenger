c8806464a975f718fc41a4169a1bc7c88d804446
vinsonlaiono <vinson.aiono@gmail.com>
2022-06-22 09:58:49 -0600

    hello

---
+++ index.js
@@ -0,0 +1,31 @@
+const firstName = 'Vinson';
+
+let age = 29;
+
+// console.log(firstName, age);
+
+let isCool = true;
+
+let x = 4;
+let y = 10;
+let z = 7+8;
+let sum = x + y + z;
+
+// if(x > y){
+//     console.log(x + " is greater than " + y)
+// } else {
+//     console.log(`${y} is greater than ${x}`)
+// }
+
+// console.log("This is the sum: ", sum)
+
+if( age != 30){
+    console.log("Hooray")
+}
+if(age > 30){
+    console.log("Im still young")
+}
+if(age < 30){
+    console.log("Im am young")
+}
+
